be388aa4976daf8b23ee1f88d1edcef3b80217a7
15719298615 <1024941594@qq.com>
2019-10-15 10:17:57 +0800

    根据数据库生成对应的mapper和pojo

---
+++ generatorSqlmapCustom/src/com/attach/sign_in/pojo/Statistics.java
@@ -0,0 +1,33 @@
+package com.attach.sign_in.pojo;
+
+public class Statistics {
+    private Integer id;
+
+    private Integer signInId;
+
+    private Integer count;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSignInId() {
+        return signInId;
+    }
+
+    public void setSignInId(Integer signInId) {
+        this.signInId = signInId;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
